c6f00d793ce6155ddfa2dc2a87bbae30343acfa5
yunju <hsiehyunju@gmail.com>
2025-02-17 10:47:50 +0800

    Initial Commit

---
+++ CathaybkHomeworkTests/CathaybkHomeworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  CathaybkHomeworkTests.swift
+//  CathaybkHomeworkTests
+//
+//  Created by YUNJU on 2025/2/17.
+//
+
+import Testing
+@testable import CathaybkHomework
+
+struct CathaybkHomeworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
